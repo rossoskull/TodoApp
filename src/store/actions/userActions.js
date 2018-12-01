9ea4e676c0819da65dbb293eb78c49fe587a56b3
Jay Mistry <mistry.jay15@gmail.com>
2018-12-01 23:25:46 +0530

    Added validation for Signup form

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -27,20 +27,27 @@ export const logoutUser = () => {
 
 export const signUp = user => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
-        const firebase = getFirebase()
-        firebase.auth().createUserWithEmailAndPassword(
-            user.email,
-            user.password
-        ).then(res => {
-            return getFirestore().collection('users').doc(res.user.uid).set({
-                fname: user.fname,
-                lname: user.lname
+
+        if ( user.fname === '' || user.fname === null ) {
+            dispatch({ type: 'SIGNUP_ERROR', err: {message: 'First name is required.'} })
+        } else if ( user.lname === '' || user.lname === null ) {
+            dispatch({ type: 'SIGNUP_ERROR', err: {message: 'Last name is required.'} })
+        } else {
+            const firebase = getFirebase()
+            firebase.auth().createUserWithEmailAndPassword(
+                user.email,
+                user.password
+            ).then(res => {
+                return getFirestore().collection('users').doc(res.user.uid).set({
+                    fname: user.fname,
+                    lname: user.lname
+                })
+            }).then(() => {
+                dispatch({ type: 'SIGNUP_SUCCESS' })
+            }).catch(err => {
+                dispatch({ type: 'SIGNUP_ERROR', err })
             })
-        }).then(() => {
-            dispatch({ type: 'SIGNUP_SUCCESS' })
-        }).catch(err => {
-            dispatch({ type: 'SIGNUP_ERROR', err })
-        })
+        }        
     }
 }
 
